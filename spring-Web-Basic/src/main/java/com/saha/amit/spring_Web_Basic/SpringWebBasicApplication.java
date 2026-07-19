f15d4a6c4b97b02e4adf8566855c16562624679d
Amit Saha <justamitsaha@gmail.com>
2026-07-19 14:31:52 +0530

    Back up

--- spring-Web-Basic/src/main/java/com/saha/amit/spring_Web_Basic/SpringWebBasicApplication.java
+++ spring-Web-Basic/src/main/java/com/saha/amit/spring_Web_Basic/SpringWebBasicApplication.java
@@ -1,8 +1,11 @@
 package com.saha.amit.spring_Web_Basic;
 
+import com.saha.amit.spring_Web_Basic.config.AppSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppSettings.class)
 @SpringBootApplication
 public class SpringWebBasicApplication {
 
